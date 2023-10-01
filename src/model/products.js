85b0f2a288b40454da7c2b54618567ab8e6ca406
Hex <hexmusictheory@protonmail.com>
2023-10-01 16:13:24 +0100

    finish first challenge

--- src/model/products.js
+++ src/model/products.js
@@ -1,1 +1,19 @@
 const db = require("../database/db.js");
+
+module.exports = { listProducts };
+
+const select_products = db.prepare(/*sql*/ `
+    SELECT
+        id,
+        name,
+        quantity_per_unit,
+        FORMAT('£%.2f', unit_price) AS unit_price,
+        units_in_stock,
+        FORMAT('£%.2f', unit_price * units_in_stock) AS stock_value,
+        units_on_order
+    FROM products
+`);
+
+function listProducts() {
+    return select_products.all();
+}
